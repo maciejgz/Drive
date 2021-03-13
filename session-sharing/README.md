1bbaf906e8128e0fecfbdf85cf3b48a1663d0623
maciejgz <macgzi@gmail.com>
2021-03-13 19:51:03 +0100

    Session sharing between two instances

--- session-sharing/README.md
+++ session-sharing/README.md
@@ -1,12 +1,15 @@
 Spring session with redis tests.
 Setup:
-- redis setup in docker
-- run the app
+- redis setup in docker (ports should be correctly configured)
+- run two instances of the app on port 8081 and 8082 
 - run tests
+ 
+user: admin/password
 
 How it works?
 
-Session created with basic auth is added to the redis and then can be taken by the user.
+Session created with basic auth is added to  redis and then can be taken by the user.
 Spring security is using Redis as the session storage db.
-Test checks if the rest template call with credentials saves JSESSION ID in the REDIs and then cleanup database.
-After that, the same call with JSESSIONID is performed and user should not be logged out. 
+Test checks if the rest template call with credentials saves JSESSIONID in the REDIS and then cleanup database.
+After that, the same call with previous JSESSIONID is performed and user should not be logged out. 
+Second main test checks if session is propagated to all app instances using the same SESSIONID in redis.
--- session-sharing/src/test/java/pl/mg/drive/sessionsharing/SessionControllerE2ETest.java
+++ session-sharing/src/test/java/pl/mg/drive/sessionsharing/SessionControllerE2ETest.java
@@ -3,8 +3,7 @@ package pl.mg.drive.sessionsharing;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import redis.clients.jedis.Jedis;
@@ -14,38 +13,44 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SessionControllerTest {
+public class SessionControllerE2ETest {
 
     private Jedis jedis;
     private static TestRestTemplate testRestTemplate;
+    private static TestRestTemplate testRestTemplateSecondInstance;
     private static TestRestTemplate testRestTemplateWithAuth;
-    private String testUrl = "http://localhost:8081/";
+    private final String testUrl = "http://localhost:8081/";
+    private final String testUrlSecondInstance = "http://localhost:8082/";
 
     @BeforeClass
     public static void initRestTemplate() {
         testRestTemplate = new TestRestTemplate();
-        testRestTemplateWithAuth = new TestRestTemplate("admin", "password", null);
+        testRestTemplateWithAuth = new TestRestTemplate("admin", "password");
+        testRestTemplateSecondInstance = new TestRestTemplate();
     }
 
     @Before
     public void clearRedisData() {
-        jedis = new Jedis("localhost", 6379);
+        jedis = new Jedis("192.168.99.102", 6379);
         jedis.flushDB();
     }
 
     @Test
+    @DisplayName(value = "Verify redis connection and emptiness")
     public void testRedisIsEmpty() {
         Set<String> result = jedis.keys("*");
         assertEquals(0, result.size());
     }
 
     @Test
+    @DisplayName(value = "unauthenticated user access to the protected resource")
     public void testUnauthenticatedCantAccess() {
         ResponseEntity<String> result = testRestTemplate.getForEntity(testUrl, String.class);
         assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
     }
 
     @Test
+    @DisplayName(value = "login to one instance, pass JSESSIONID to another request, access without JSESSIONID")
     public void testRedisControlsSession() {
         ResponseEntity<String> result = testRestTemplateWithAuth.getForEntity(testUrl, String.class);
         assertEquals("hello admin", result.getBody()); //login worked
@@ -55,7 +60,6 @@ public class SessionControllerTest {
 
         String sessionCookie = result.getHeaders().get("Set-Cookie").get(0).split(";")[0];
         HttpHeaders headers = new HttpHeaders();
-        System.out.println("session cookie: " + sessionCookie);
         headers.add("Cookie", sessionCookie);
         HttpEntity<String> httpEntity = new HttpEntity<>(headers);
 
@@ -67,6 +71,24 @@ public class SessionControllerTest {
         result = testRestTemplate.exchange(testUrl, HttpMethod.GET, httpEntity, String.class);
         assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
         //access denied after sessions are removed in redis
+    }
+
+    @Test
+    @DisplayName(value = "pass session to another instance")
+    public void testPassingAuthenticationToAnotherInstance() {
+        ResponseEntity<String> result = testRestTemplateWithAuth.getForEntity(testUrl, String.class);
+        assertEquals("hello admin", result.getBody()); //login to first instance
+
+        Set<String> redisResult = jedis.keys("*");
+        assertTrue(redisResult.size() > 0); //redis is populated with session data
+
+        String sessionCookie = result.getHeaders().get("Set-Cookie").get(0).split(";")[0];
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", sessionCookie);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        result = testRestTemplateSecondInstance.exchange(testUrlSecondInstance, HttpMethod.GET, httpEntity, String.class);
+        assertEquals("hello admin", result.getBody());
     }
 
 }
