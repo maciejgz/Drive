ca0b0064fdffb1ee40e199eab475d415d6a425f3
mgzik <mgzik@ospartner.pl>
2021-03-20 09:16:13 +0100

    #1 monolith app init commit

---
+++ drive-monolith/src/main/java/pl/mg/drivemonolith/DriveMonolithApplication.java
@@ -0,0 +1,13 @@
+package pl.mg.drivemonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveMonolithApplication.class, args);
+    }
+
+}
---
+++ drive-monolith/src/test/java/pl/mg/drivemonolith/DriveMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mg.drivemonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
