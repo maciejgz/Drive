3dd68a4a7cc32bbfe6a207fdf0ef674be5ec8581
maciejgz <macgzi@gmail.com>
2021-04-09 15:04:24 +0200

    docker image of the express API sample

---
+++ express-api/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json([{
+        name: 'Bob',
+        email: 'bob@gmail.com'
+    },
+        {
+            name: 'Alica',
+            email: 'alice@gmail.com'
+        },
+        {
+            name: 'Jack',
+            email: 'jack@gmail.com'
+        },
+        {
+            name: 'Mark',
+            email: 'mark@gmail.com'
+        }
+
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
