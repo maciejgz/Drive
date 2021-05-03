21793f2921250d387ab044ce73af15ac650ae1b3
maciejgz <macgzi@gmail.com>
2021-05-03 11:50:55 +0200

    nodejs websocket server dockerized

---
+++ websocket/client.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws')
+const url = 'ws://192.168.99.102:8081'
+const connection = new WebSocket(url)
+
+connection.onopen = () => {
+    connection.send('Ping From Client')
+    connection.close();
+}
+
+connection.onerror = (error) => {
+    console.log(`WebSocket error: ${error}`)
+}
+
+connection.onmessage = (e) => {
+    console.log(e.data)
+}
