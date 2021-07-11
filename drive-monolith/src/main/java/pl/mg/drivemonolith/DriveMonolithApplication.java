445434688d5f9ba15b1e705da041d8604fe5db98
maciejgz <macgzi@gmail.com>
2021-07-11 09:45:09 +0200

    #1 sample test with H2 database

--- drive-monolith/src/main/java/pl/mg/drivemonolith/DriveMonolithApplication.java
+++ drive-monolith/src/main/java/pl/mg/drivemonolith/DriveMonolithApplication.java
@@ -3,10 +3,12 @@ package pl.mg.drivemonolith;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableTransactionManagement
 //@EnableAspectJAutoProxy
 @Slf4j
 public class DriveMonolithApplication {
