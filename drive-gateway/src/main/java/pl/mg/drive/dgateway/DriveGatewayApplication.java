bf7a3109deed973b4a78dc911e9e63cb108fe11d
maciejgz <macgzi@gmail.com>
2022-05-03 20:51:27 +0200

    #3 spring cloud gateway with Keycloack integration and custom scope defined in keycloak

--- drive-gateway/src/main/java/pl/mg/drive/dgateway/DriveGatewayApplication.java
+++ drive-gateway/src/main/java/pl/mg/drive/dgateway/DriveGatewayApplication.java
@@ -3,9 +3,11 @@ package pl.mg.drive.dgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableWebFluxSecurity
 public class DriveGatewayApplication {
 
     public static void main(String[] args) {
---
+++ drive-journey/src/main/java/pl/mg/drive/journey/controller/JourneyController.java
@@ -0,0 +1,25 @@
+package pl.mg.drive.journey.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/journey")
+public class JourneyController {
+
+
+    @PreAuthorize("hasAuthority('SCOPE_TEST')")
+    @GetMapping("/ping")
+    public String ping() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        return "Scopes: " + authentication.getAuthorities();
+    }
+
+}
